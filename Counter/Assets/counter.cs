3eefd6c7f0ed717c96758286a10399828cff8d20
feaxmutch <feaxmutch@gmail.com>
2024-04-05 14:14:20 +0300

    Added project Counter

---
+++ Counter/Assets/counter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class counter : MonoBehaviour
+{
+    private bool _isRunning = true;
+
+    [SerializeField] private Text _text;
+    [SerializeField] private float _delay;
+
+    private void Start()
+    {
+        StartCoroutine(RunCounter(_delay));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isRunning = !_isRunning;
+        }
+    }
+
+    private IEnumerator RunCounter(float delay)
+    {
+        int count = 0;
+        var wait = new WaitForSeconds(delay);
+
+        while (true)
+        {
+            DisplayCount(count);
+            yield return wait;
+            yield return new WaitUntil(() => _isRunning);
+            count++;
+        }
+    }
+
+    private void DisplayCount(int count)
+    {
+        _text.text = count.ToString();
+    }
+}
